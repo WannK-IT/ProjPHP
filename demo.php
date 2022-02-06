b6b9405699531f6d16299c774544d71d9d88412e
WannK-IT <n.nquanght@gmail.com>
2022-02-06 17:31:18 +0700

    rename folder

--- demo.php
+++
@@ -1,4 +0,0 @@
-//If exist this session => show alert(register_success)
-                            if (isset($_SESSION['loginFail'])) {
-                                echo '<div style="position: absolute; top: 10px; right: 10px" class="alert alert-danger" id="loginFail" role="alert">' . $_SESSION['loginFail'] . '</div>';
-                            }
